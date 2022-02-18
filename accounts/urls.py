d6b5c7358c11b3f22d02056d6111cd20b77faef5
kangwhi <kkh8123@naver.com>
2022-02-18 22:44:38 +0900

    feat: SignUp 기능 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_view
+from .views import *
 
 # profile 페이지가 없다... 해결방법은?
 # 1. profile 만들기
@@ -8,5 +9,10 @@ from django.contrib.auth import views as auth_view
 #   b. 웹 서버에서 설정(redirect, 다른 페이지로 보내줌)
 urlpatterns = [
     path("login/", auth_view.LoginView.as_view(), name="login"),
-    path("logout/", auth_view.LogoutView.as_view(template_name='registration/logout.html'), name="logout"),
+    path(
+        "logout/",
+        auth_view.LogoutView.as_view(template_name="registration/logout.html"),
+        name="logout",
+    ),
+    path("register/", register, name="register"),
 ]
